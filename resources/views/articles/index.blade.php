ac1880a62d4d7fce1d50f6c35c8c4fa1951c3f02
evgeniy <uhvatkin.e@gigwork.ru>
2025-09-28 23:11:47 +0300

    crud чз роутер

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', 'Articles')
+
+@section('content')
+    <h1>Articles</h1>
+
+    @if($articles->count())
+        <div class="row">
+            @foreach($articles as $article)
+                <div class="col-md-6 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $article->name }}</h5>
+                            <p class="card-text">{{ Str::limit($article->body, 100) }}</p>
+                            <small class="text-muted">
+                                Created: {{ $article->created_at->format('d.m.Y') }}
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <!-- Пагинация -->
+        <div class="mt-4">
+            {{ $articles->links() }}
+        </div>
+    @else
+        <div class="alert alert-info">
+            No articles found.
+        </div>
+    @endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,7 @@ Route::get('/', function () {
 //    return view('about');
 //})->name('about');
 Route::get('about', [PageController::class, 'about'])->name('about');
+
+// Название сущности в URL во множественном числе, контроллер в единственном
+Route::get('articles', [ArticleController::class, 'index'])
+    ->name('articles.index'); // имя маршрута, нужно для того, чтобы не создавать ссылки руками
